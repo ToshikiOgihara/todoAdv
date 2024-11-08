647bd7e48d6744193742bf6d2eaa17860a7101b3
Toshiki_Ogihara <toshiki.ogihara14@gmail.com>
2024-11-08 19:52:05 +0000

    Todoを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,29 @@
 class TasksController < ApplicationController
   def index
+    #@articles = Article.order(id: :desc).page(params[:page]).per(3)
+    @tasks = Task.order(id: :asc)
   end
   
   def new
+    if params[:button]
+      render :_new_button
+    else
+      @task = Task.new
+    end
   end
+  
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      render :create
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+    def task_params
+      params.require(:task).permit(:title)
+    end
 end
